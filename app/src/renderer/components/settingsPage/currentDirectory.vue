cc9bf1e12dadc3e4cd6092e4f275b6786860eb91
Eddie Stanley <acklavidian@gmail.com>
2017-04-22 21:27:55 -0500

    adding component for current dir

---
+++ app/src/renderer/components/settingsPage/currentDirectory.vue
@@ -0,0 +1,23 @@
+<template>
+    <md-input-container md-inline>
+      <label> owner:  </label>
+      <md-input type='text' @click.native="setDirectory" :value="directory"></md-input>
+    </md-input-container>
+</template>
+
+<script>
+import * as types from '../../vuex/mutation-types'
+
+export default {
+  computed: {
+    directory: data => data.$store.state.userSettings.owner
+  },
+  methods: {
+    setDirectory (value) {
+      const {dialog} = require('electron').remote
+      let directory = dialog.showOpenDialog({properties: ['openFile', 'openDirectory', 'multiSelections']})
+      this.$store.commit(types.GITHUB_SET_OWNER, directory)
+    }
+  }
+}
+</script>
